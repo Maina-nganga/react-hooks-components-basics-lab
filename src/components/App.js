13862f310f1e5936f5a4951a145e4d59f9e80608
Maina Nganga <ngangamaina091@gmail.com>
2025-10-28 22:03:54 +0300

    solve few bugs

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,14 @@
 import React from "react";
-
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
-}
-
-function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
-    </div>
-  );
-}
-
-{/* write an <About> component here */}
-
+import Home from "./Home";
+import About from "./About";
+import user from "../data/user";
 
 function App() {
   return (
     <div>
-      <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <nav></nav>
+      <Home name={user.name} city={user.city} color={user.color} />
+      <About bio={user.bio} links={user.links} />
     </div>
   );
 }
